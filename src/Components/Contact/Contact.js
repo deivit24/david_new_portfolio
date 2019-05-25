3895420fcbfdad8a0e66c54db835fc3b6eced9fd
David Salazar <david.asal@hotmail.com>
2019-05-25 14:27:05 -0500

    added react-netlify-fomrs npm

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,51 +1,113 @@
 import React from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
+import NetlifyForm from 'react-netlify-form';
 
-const encode = data => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-};
 class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      number: '',
-      message: ''
-    };
-  }
-
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...this.state })
-    });
-
-    e.preventDefault();
-  };
-
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
   render() {
-    const { firstName, lastName, email, number, message } = this.state;
     return (
       <div>
         <section id="sec5" className="demo">
           <h1 id="contact">Let's Talk!</h1>
           <div className="container">
-            <form
-              onSubmit={this.handleSubmit}
-              method="POST"
-              data-netlify="true"
-              name="contact"
+            <NetlifyForm
+              className="NetlifyForm"
+              name="Form With Recaptcha"
+              recaptcha={{
+                sitekey: 'my_recaptcha_site_key',
+                size: 'normal'
+              }}
             >
-              <input type="hidden" name="form-name" value="contact" />
+              {({ loading, error, recaptchaError, success, recaptcha }) => (
+                <div>
+                  {loading && <div>Loading...</div>}
+                  {error && (
+                    <div>
+                      Your information was not sent. Please try again later.
+                    </div>
+                  )}
+                  {recaptchaError && (
+                    <div>
+                      Recaptcha did not match. Please make sure the box is
+                      checked.
+                    </div>
+                  )}
+                  {success && <div>Thank you for contacting us!</div>}
+                  {!loading && !success && (
+                    <div>
+                      <div className="form-row">
+                        <div className="form-group col-md-6">
+                          <label>First Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="first-name"
+                            placeholder="John"
+                            name="firstName"
+                            required
+                          />
+                        </div>
+                        <div className="form-group col-md-6">
+                          <label>Last Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="last-name"
+                            placeholder="Smith"
+                            name="lastName"
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className="form-row">
+                        <div className="form-group col-md-6">
+                          <label>Email address</label>
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter email"
+                            name="email"
+                            required
+                          />
+                          <small
+                            id="emailHelp"
+                            className="form-text text-muted"
+                          >
+                            We'll never share your email with anyone else.
+                          </small>
+                        </div>
+                        <div className="form-group col-md-6">
+                          <label>Number</label>
+                          <input
+                            type="number"
+                            className="form-control"
+                            id="number"
+                            placeholder="555-555-5555"
+                            name="number"
+                          />
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <label>Personal Message</label>
+                        <textarea
+                          className="form-control"
+                          id="message"
+                          rows="3"
+                          name="message"
+                          placeholder="Enter a personal message"
+                        />
+                        {recaptcha}
+                      </div>
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </NetlifyForm>
+            {/* <NetlifyForm name="Contact">
               <div className="form-row">
                 <div className="form-group col-md-6">
                   <label>First Name</label>
@@ -55,9 +117,7 @@ class Contact extends React.Component {
                     id="first-name"
                     placeholder="John"
                     name="firstName"
-                    value={firstName}
                     required
-                    onChange={this.handleChange}
                   />
                 </div>
                 <div className="form-group col-md-6">
@@ -69,8 +129,6 @@ class Contact extends React.Component {
                     placeholder="Smith"
                     name="lastName"
                     required
-                    value={lastName}
-                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -84,8 +142,6 @@ class Contact extends React.Component {
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
                     name="email"
-                    value={email}
-                    onChange={this.handleChange}
                     required
                   />
                   <small id="emailHelp" className="form-text text-muted">
@@ -100,8 +156,6 @@ class Contact extends React.Component {
                     id="number"
                     placeholder="555-555-5555"
                     name="number"
-                    value={number}
-                    onChange={this.handleChange}
                   />
                 </div>
               </div>
@@ -113,14 +167,12 @@ class Contact extends React.Component {
                   rows="3"
                   name="message"
                   placeholder="Enter a personal message"
-                  value={message}
-                  onChange={this.handleChange}
                 />
               </div>
               <button type="submit" className="btn btn-primary">
                 Submit
               </button>
-            </form>
+            </NetlifyForm> */}
             <div className="icons">
               <a href="https://www.facebook.com/AugieSal" target="a_blank">
                 <i className="fab fa-facebook fa-2x" />
